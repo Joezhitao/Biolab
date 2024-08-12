fd37eb92b1d331c5914b37e0f46504b00a53f18d
Joezhitao <935423432@qq.com>
2024-08-12 11:33:14 +0800

    modify load Biolab

--- GEO数据整理.R
+++ GEO数据整理.R
@@ -29,3 +29,64 @@ pd=pData(gset[[1]])
 #输出临床信息
 write.csv(pd,'clinical_GSE13067.csv',row.names = TRUE)
 
+#读入txt注释文件
+gpl=read.table("GPL570-55999.txt",
+               header = TRUE,fill = T,sep = "\t",
+               comment.char = "#",
+               stringsAsFactors = FALSE,
+               quote = "")
+gpl$Gene.Symbol
+#查看
+#View(gpl)
+#colnames(gpl)
+
+
+#提取探针ID及基因symbol
+ids=gpl[,c("ID","Gene.Symbol")]
+
+#修改列名
+colnames(ids)=c('probe_id','symbol')
+
+#获取基因symbol
+library(stringr)  
+#[]中的数字，以及///的数量，根据表格中gene symbol的位置决定
+ids$symbol=trimws(str_split(ids$symbol,'///',simplify = T)[,1])
+
+#去掉没有注释symbol的探针
+ids=ids[ids$symbol != '',]
+ids=ids[ids$symbol != '---',]
+ids=ids[ids$symbol != '--- ',]
+
+#由于部分探针ID是由全数字组成的，会导致R自动将其识别为数值型
+#因此，这里需要将探针的id改为字符型
+rownames(dat) = as.character(rownames(dat))
+ids$probe_id = as.character(ids$probe_id)
+
+#平台文件的ID和矩阵中的ID匹配。%in%用于判断是否匹配。
+ids=ids[ids$probe_id %in% rownames(dat),]
+
+#获取匹配的表达数据
+dat=dat[ids$probe_id,]
+
+#查看探针名与注释文件名是否完全一致
+table(rownames(dat) == ids$probe_id)
+
+#合并
+dat <- cbind(ids,dat)
+
+#删除重复基因，保留最大值
+dat <- aggregate( . ~ symbol,data=dat, max)
+
+#一定注意看，是否需要删除几行
+View(dat)
+#dat = dat[-1,]
+
+#转化行名
+rownames(dat) <- dat[,1]
+
+#删除第一二列
+dat <- dat[,-c(1,2)]
+
+#导出
+write.table(data.frame(ID=rownames(dat),dat),file="GSE13067.txt", sep="\t", quote=F, row.names = F)
+
