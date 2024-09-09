a2d679a344927b3cb0110f8dbda3ae20f84b42ec
Joezhitao <935423432@qq.com>
2024-09-10 01:50:35 +0800

    modify

--- B组气泡火山图.R
+++ B组气泡火山图.R
@@ -19,10 +19,11 @@ genepath = 'E:/B组数据备份(4.29)/横向比较基因/'
 #seurat对象设置
 filepath <- paste("E:/B组数据备份(4.29)/去污染后细胞亚群备份/",'Hepatocytes',".RDS", sep = "")
 pbmc <- readRDS(filepath)
+sce <- pbmc
 #抽组
 group = c("Ad1R","Bd1R","Bd8R","Bd15R","Bd30R")
 
-sce <- pbmc
+
 
 for (i in group) {
   pbmc <- sce
@@ -62,15 +63,27 @@ for (i in group) {
   dev.off()
 }
 
+gene <- read.xlsx(paste0(genepath,"5265.xlsx", sep = ""),sheetIndex = 1,header = T,encoding = "UTF-8")
+gene <- c(gene$gene)
+plotfile <- paste("E:/B组数据备份(4.29)/单细胞结果/气泡图/气泡再生基因_ccn",".pdf", sep = "")
+plot <- DotPlot(pbmc, features = gene,group.by = 'seurat_clusters',dot.scale = 8)+
+  theme_bw()+
+  theme(panel.grid = element_blank(), 
+        axis.text.x=element_text(hjust = 1, vjust=0.5, angle=90))+
+  labs(x=NULL,y=NULL)+guides(size=guide_legend(order=3))+
+  scale_color_gradientn(values = seq(0,1,0.2),colours = c('#330066','#336699','#66CC66','#FFCC33')) +
+  scale_y_discrete(limits = rev(levels(pbmc@meta.data$seurat_clusters)))
+
+pdf(plotfile, width = 8, height = 5)
+print(plot)
+dev.off()
+
 ###############################################################################
 #肝细胞空白上调基因
 library(Seurat) 
 library(tidyverse)
 library(scRNAtoolVis)
-devtools::install_github('junjunlab/jjPlot')
-devtools::install_github("sajuukLyu/ggunchull", type = "source")
-install.packages('geomtextpath')
-install.packages('ggh4x')
+
 group = c("Ad1R","Bd1R","Bd8R","Bd15R","Bd30R")
 #group减少"Ad1R"
 group = c("Bd1R","Bd8R","Bd15R","Bd30R")
@@ -119,13 +132,83 @@ for (j in group) {
   print(vol)
   dev.off()
 }
+#seurat对象设置
+filepath <- paste("E:/B组数据备份(4.29)/去污染后细胞亚群备份/",'Hepatocytes',".RDS", sep = "")
+pbmc <- readRDS(filepath)
 
+# find markers
+pbmc.markers <- FindAllMarkers(pbmc, only.pos = FALSE,
+                               min.pct = 0.25,
+                               logfc.threshold = 0)
+path = paste("E:/B组数据备份(4.29)/单细胞结果/火山图/Volcano",".pdf", sep = "")
 
-file = "E:/B组数据备份(4.29)/单细胞结果/去污染/combined_all_markers.csv"
-combined_markers = read.csv(file,header = T,stringsAsFactors = F)
-path = paste("E:/B组数据备份(4.29)/单细胞结果/火山图/Volcano",".png", sep = "")
+pdf(path, width = 14, height = 12)
+jjVolcano(diffData = pbmc.markers,topGeneN = 30)
+dev.off()
 
-color.pals = c("#DC143C","#0000FF","#20B2AA","#FFA500","#9370DB","#98FB98","#F08080","#1E90FF","#7CFC00","#FFFF00",
-               "#808000","#FF00FF","#FA8072","#7B68EE","#9400D3","#800080","#A0522D","#D2B48C","#D2691E","#87CEEB","#40E0D0","#5F9EA0",
-               "#FF1493","#0000CD","#008B8B","#FFE4B5","#8A2BE2","#228B22","#E9967A","#4682B4","#32CD32","#F0E68C","#FFFFE0","#EE82EE",
-               "#FF6347","#6A5ACD","#9932CC","#8B008B","#8B4513","#DEB887")
+###############################################################################
+##横向小提琴图
+#抽组
+filepath = "E:/B组数据备份(4.29)/单细胞结果/横向比较/组间/"
+group = c("Ad1R","Bd1R","Bd8R","Bd15R","Bd30R")
+#sce <- pbmc
+for (i in group) {
+  pbmc <- sce
+  pbmc = pbmc[, pbmc@meta.data$group %in% i]#抽组
+  pbmc@meta.data$group <- droplevels(pbmc@meta.data$group)
+  
+  mouse_gene <- read.xlsx(paste0(genepath,"5265.xlsx", sep = ""),sheetIndex = 1,header = T,encoding = "UTF-8")
+  mouse_gene <- c(mouse_gene$gene)
+  
+  #画图
+  plot <- VlnPlot(pbmc, mouse_gene, stack = TRUE,group.by = "seurat_clusters",
+                  sort = FALSE, flip = TRUE, cols = color.pals) +
+    theme(legend.position = "none")
+  
+  pdf(paste0(filepath,"ViolinPlot_",i,".pdf",sep = ""), width = 15, height = 40)
+  print(plot)
+  dev.off()
+}
+################################################################################
+#featureplot
+#每一个基因上跑
+
+mouse_gene <- read.xlsx(paste0(genepath,"5265.xlsx", sep = ""),sheetIndex = 1,header = T,encoding = "UTF-8")
+mouse_gene <- c(mouse_gene$gene)
+for (i in mouse_gene) {
+  filepath = "E:/B组数据备份(4.29)/单细胞结果/横向比较/组间/"
+  gene <- i
+  gene_name = paste("肝细胞组间","_",i, sep = "")
+  pbmc_hep <- PercentageFeatureSet(sce,features = gene,col.name = gene_name)
+  path2 = paste(filepath,"UAMP_",gene_name,".pdf", sep = "")
+  p2 <- FeaturePlot(pbmc_hep,gene_name)
+  pdf(path2, width = 8, height = 8)
+  print(p2)
+  dev.off()
+  
+}
+################################################################################
+gene_name = paste("肝细胞组间","_",'衰老基因集', sep = "")
+#选择seurat对象中有的基因
+mouse_gene <- mouse_gene[mouse_gene %in% rownames(sce)]
+pbmc_hep <- PercentageFeatureSet(sce,features = mouse_gene,col.name = gene_name)
+
+factors <- c(levels(pbmc_hep@meta.data$group))
+combinations <- combn(factors, 2) 
+combinations_list <- split(combinations, rep(1:ncol(combinations), each = nrow(combinations)))
+library(ggpubr)
+my9color <- c('#5470c6','#91cc75','#fac858','#ee6666','#73c0de')
+filepath = "E:/B组数据备份(4.29)/单细胞结果/横向比较/组间/"
+path = paste(filepath,"组间横向比较_",gene_name,".pdf", sep = "")
+p.percentage <- ggviolin(pbmc_hep@meta.data, x = "group", y = gene_name,
+                         color = "group",add = 'mean_sd',fill = 'group',
+                         add.params = list(color = "black")) + 
+  stat_compare_means(comparisons = combinations_list,label = "p.signif") + 
+  scale_color_manual(values = my9color) + 
+  scale_fill_manual(values = my9color) +
+  theme(axis.text.x.bottom = element_text(angle = 90,vjust = 0.5,hjust = 1)) +
+  #ylim(-0.2, 0.1) +
+  NoLegend() + labs(x = '')
+pdf(path, width = 8, height = 8)
+print(p.percentage)
+dev.off()
